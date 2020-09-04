dcf294c684777b6f3f4908b7a70018cafe52dfb3
roylee0704 <roylee0704@gmail.com>
2020-09-04 09:30:06 +0700

    refac: convert to function component with react-hook

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+export default () => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Roy', age: 30 },
       { name: 'Earng', age: 27 },
       { name: 'SanSan', age: 0.6 }
     ]
-  };
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: 'Roy Lee', age: 31 },
         { name: 'Earng', age: 27 },
         { name: 'SanSan Lee', age: 0.6 }
       ]
-    })
+    });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I'm a React App</h1>
-        <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+  return (
+    <div className="App">
+      <h1>Hi I'm a React App</h1>
+      <p>This is really working</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobby: Reading books..</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-  }
-}
-
-export default App;
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Hobby: Reading books..</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
+};
