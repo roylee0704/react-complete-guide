79f0060c0e22b4e54e03b55e760eb1c0f1e40bba
roylee0704 <roylee0704@gmail.com>
2020-09-05 13:55:22 +0700

    feat: style with stylesheet

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+// webpack will pick this up and inject to html
+// .css should be expressed explicitly, don't ignore.
+import './Person.css';
+
 export default (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>My name is {props.name}, I'm {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name} />
