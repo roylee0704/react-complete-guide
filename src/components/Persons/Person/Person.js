0af73fc4aa0d50addf5dae578c5d534fad0adf4f
roylee0704 <roylee0704@gmail.com>
2020-09-09 08:30:52 +0700

    feat: useRef and useEffect

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 // webpack will pick this up and inject to html
 // .css should be expressed explicitly, don't ignore.
@@ -7,12 +7,24 @@ import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 
 const Person = (props) => {
+
+    const inputEl = useRef(null);
+
+    useEffect(() => {
+        inputEl.current.focus();
+    }, []);
+
     console.log('[Person] rendering...')
     return (
         <Aux>
             <p onClick={props.click}>My name is {props.name}, I'm {props.age} years old.</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.change} value={props.name} />
+            <input
+                type="text"
+                onChange={props.change}
+                value={props.name}
+                ref={inputEl}
+            />
         </Aux>
     );
 };
